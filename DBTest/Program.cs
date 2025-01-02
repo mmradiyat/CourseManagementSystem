2ad175da42c4cf46cd2a6231c3143863c86380d7
mmradiyat <15103186@iubat.edu>
2025-01-02 15:31:08 +0600

    all

--- DBTest/Program.cs
+++ DBTest/Program.cs
@@ -7,7 +7,30 @@ namespace DBTest
 {
     internal class Program
     {
-      
-        
+        class DBInsertTest
+        {
+            public DBInsertTest()
+            {
+                var db = new CourseManagementSystemContext();
+                Console.WriteLine(DateTime.Now);
+
+                User myUser = new User();
+                myUser.UserName = null;
+                myUser.UserEmail = "admin@admin.admin";
+                myUser.UserType = "admin";
+                myUser.UserPasswordHash = "Hash";
+                myUser.IsUserActive = true;
+
+                db.User.Add(myUser);
+                int row = db.SaveChanges();
+            }
+
+            static void Main(string[] args)
+            {
+                CourseManagementSystemContext db = new CourseManagementSystemContext();
+
+                var AllUser = db.User.ToList();
+            }
+        }
     }
-};
+}
